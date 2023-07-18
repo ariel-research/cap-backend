701e0bd1442a8bd3a1e7439b1c1ef5cb6f8ed524
oriyalp <oriya2011@gmail.com>
2023-07-18 21:35:05 +0300

    Edit run-backend script

--- run-backend.py
+++ run-backend.py
@@ -1,23 +1,29 @@
 import os
-import subprocess
+import sys
 from dotenv import load_dotenv
-
 # Load environment variables from .env file
 load_dotenv()
 
-# Get the hostname and port from command line arguments
-hostname = os.getenv("HOSTNAME")
-port = os.getenv("PORT_BACKEND")
+PID_NUM_FILE = os.getenv("PID_NUM_FILE")
 
-# Build the command to run the Django development server
-run_django = f'nohup python manage.py runserver {hostname}:{port} &'
+if sys.argc == 2:
+    command = sys.argv[0]
+    if command == 'run':
+        # Get the hostname and port from command line arguments
+        hostname = os.getenv("HOSTNAME")
+        port = os.getenv("PORT_BACKEND")
 
-# Activate the virtual environment
-venv_activate = 'source venv/bin/activate'
+        # Build the command to run the Django development server
+        run_django = f'nohup python3.10 manage.py runserver {hostname}:{port} output.log 2>&1 &'
+        save_pid = 'echo $! > save_pid.txt'
 
-# Build the full command
-command = f'{venv_activate} && {run_django}'
+        # Run the command using subprocess and redirect the output to a log file
+        os.system(run_django)
+        os.system(save_pid)
 
-# Run the command using subprocess and redirect the output to a log file
-#subprocess.Popen(command, shell=True, stdout=open('backend.log', 'w'), stderr=subprocess.STDOUT)
-os.system(command)
+    elif command == 'stop' :
+        os.system(f'kill -9 `cat {PID_NUM_FILE}`')
+        os.system(f'rm `{PID_NUM_FILE}`')
+
+    else:
+        raise Exception("Invalid argument. Usage: ./run-backend.py [run|stop] ")
